28289bd47899f9a19a0822e845fe66393fa355bc
BTMuli <BT-Muli@outlook.com>
2022-11-10 23:44:23 +0800

    fix(request): 调整了一下，能收到数据了

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,36 +3,32 @@ import axios from 'axios';
 const BASE_URL = 'https://mock.apifox.cn/m1/1887182-0-default';
 
 export async function request(options) {
-	return new Promise((resolve, reject) => {
-		if (options.data) {
-			axios
-				.request({
-					url: BASE_URL + options.url,
-					method: options.method,
-					data: options.data,
-				})
-				.then(res => {
-					if (res.data.statusCode === 0) {
-						resolve(res.data);
-					}
-				})
-				.catch(err => {
-					reject(err.message);
-				});
-		} else {
-			axios
-				.request({
-					url: BASE_URL + options.url,
-					method: options.method,
-				})
-				.then(res => {
-					if (res.data.statusCode === 0) {
-						resolve(res.data);
-					}
-				})
-				.catch(err => {
-					reject(err.message);
-				});
-		}
-	});
+	let reqRes;
+	if (options.data) {
+		await axios
+			.request({
+				url: BASE_URL + options.url,
+				method: options.method,
+				data: options.data,
+			})
+			.then(async res => {
+				reqRes = res.data;
+			})
+			.catch(err => {
+				console.log(err)
+			});
+	} else {
+		await axios
+			.request({
+				url: BASE_URL + options.url,
+				method: options.method,
+			})
+			.then(async res => {
+				reqRes = res.data
+			})
+			.catch(err => {
+				console.log(err)
+			});
+	}
+	return reqRes;
 }
