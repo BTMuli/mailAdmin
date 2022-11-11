6fd4eb2ea8133137e34d776292b0971c3fdcd36b
BTMuli <BT-Muli@outlook.com>
2022-11-11 20:50:12 +0800

    fix(request): 添加 qs ，配置默认 content-type

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,24 +1,28 @@
 import axios from 'axios';
+import qs from 'qs';
 
 // const BASE_URL = 'https://mock.apifox.cn/m1/1887182-0-default';
 const BASE_URL = 'http://healthcode.natapp1.cc';
 
+// reqRes = { type, message, content };
+
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+
 export async function request(options) {
 	await console.log('request.options', options);
 	let reqRes;
-	await axios
-		.request({
-			url: BASE_URL + options.url,
-			method: options.method,
-			data: options.data,
-		})
-		.then(async res => {
-			await console.log('request.res', res);
-			reqRes = res.data;
-		})
-		.catch(async err => {
-			await console.log(err);
-		});
+	await axios({
+		url: BASE_URL + options.url,
+		method: options.method,
+		data: qs.stringify(options.data)
+	})
+	.then(async res => {
+		await console.log('request.res', res);
+		reqRes = res.data;
+	})
+	.catch(async err => {
+		await console.log(err);
+	});
 	await console.log('request.reqRes', reqRes);
-	return reqRes;
+	return reqRes.data;
 }
