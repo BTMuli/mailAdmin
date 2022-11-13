b5eca1114fddea3612f1b37ea3b38ed1123f4a49
BTMuli <BT-Muli@outlook.com>
2022-11-13 21:32:02 +0800

    fix(group): 完善邮件群发样式

--- src/pages/functions/groupMail.vue
+++ src/pages/functions/groupMail.vue
@@ -1,13 +1,25 @@
 <template>
 	<div>
-		<div>群发邮件界面</div>
-		<!-- todo 先测试功能完整性 -->
-		<el-form ref="groupForm" size="large">
-			<el-form-item label="内容">
-				<div class="edit_container"></div>
+		<div class="top_title">群发邮件</div>
+		<el-form ref="mailForm" size="large" v-model="mailForm">
+			<el-form-item required>
+				<el-input
+					v-model="mailForm.receiver"
+					placeholder="请输入收件人"
+				/>
 			</el-form-item>
+			<el-form-item required>
+				<el-input v-model="mailForm.title" placeholder="请输入主题" />
+			</el-form-item>
+			<el-form-item required>
+				<el-input
+					v-model="mailForm.content"
+					placeholder="请输入内容"
+					type="textarea"
+				/>
+			</el-form-item>
+			<el-button type="primary" @click="sendMail()">发送</el-button>
 		</el-form>
-		<button @click="sendMail()"></button>
 	</div>
 </template>
 
@@ -19,23 +31,52 @@
 			return {
 				mailForm: {
 					users: [],
+					title: '',
 					content: null,
-					editorOption: {},
 				},
 			};
 		},
 		methods: {
 			async sendMail() {
+        // todo 根数实际情况修改
 				let formData = {
 					users: this.mailForm.users,
+          title: this.mailForm.title,
 					content: this.mailForm.content,
 				};
 				console.log(formData);
 				const userStore = useUserStore();
-				await userStore.sendGroupMail(formData);
+        let sendData = this.transMail(formData);
+        await userStore.sendGroupMail(sendData);
 			},
+      transContent2Ascii(content) {
+        let ascii = '';
+        for (let i = 0; i < content.length; i++) {
+          ascii += content.charCodeAt(i).toString(16);
+        }
+        return ascii;
+      },
+      // todo 根据实际情况修改
+      transMail(mailData) {
+        let res = '';
+        res += 'from: example@mail.com\n';
+        res += 'to: ' + mailData.users + '\n';
+        res += 'subject: ' + mailData.title + '\n\n';
+        res += this.transContent2Ascii(mailData.content);
+        return res;
+      },
 		},
 	};
 </script>
 
-<style scoped></style>
+<style scoped>
+	.top_title {
+		height: 50px;
+		padding: 10px;
+		text-align: left;
+		font-size: 30px;
+		font-weight: normal;
+		margin-top: 10px;
+		display: inline-block;
+	}
+</style>
