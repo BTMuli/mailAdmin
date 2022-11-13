2751724c774a9cb1977be9945acc6a3e9c10f986
BTMuli <BT-Muli@outlook.com>
2022-11-13 16:55:01 +0800

    fix(app): 添加条件显示，登录注册采用不同页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
-	<!-- 布局容器 -->
-	<el-container>
-		<!-- 顶部导航栏 -->
-		<el-header>
-			<!-- 顶部导航栏组件 -->
-			<top-nav />
-		</el-header>
-		<!-- 侧边栏 -->
+	<view
+		v-show="
+			this.$route.path !== loginPath && this.$route.path !== registerPath
+		"
+	>
+		<!-- 布局容器 -->
 		<el-container>
-			<el-aside width="200px">
-				<!-- 侧边栏导航组件 -->
-				<sidebar />
-			</el-aside>
-			<!-- 主体内容 -->
-			<el-main class="right_container">
-				<!-- 路由视图 -->
-				<router-view />
-			</el-main>
+			<!-- 顶部导航栏 -->
+			<el-header>
+				<!-- 顶部导航栏组件 -->
+				<top-nav />
+			</el-header>
+			<!-- 侧边栏 -->
+			<el-container>
+				<el-aside width="200px">
+					<!-- 侧边栏导航组件 -->
+					<sidebar />
+				</el-aside>
+				<!-- 主体内容 -->
+				<el-main class="right_container">
+					<!-- 路由视图 -->
+					<router-view />
+				</el-main>
+			</el-container>
 		</el-container>
-	</el-container>
+	</view>
+	<view
+		v-show="
+			this.$route.path === loginPath || this.$route.path === registerPath
+		"
+	>
+		<router-view />
+	</view>
 </template>
 
 <script>
@@ -30,6 +43,12 @@
 	export default {
 		name: 'app',
 		components: { TopNav, Sidebar },
+		data() {
+			return {
+				loginPath: '/login',
+				registerPath: '/',
+			};
+		},
 		methods: {},
 	};
 </script>
