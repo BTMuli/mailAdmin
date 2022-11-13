b33c3a7e7577dccd4e338e38ccbd2d327a225222
BTMuli <BT-Muli@outlook.com>
2022-11-13 15:46:37 +0800

    fix(sidebar): 侧边栏导航组件样式完成

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,6 +1,14 @@
+<!-- 侧边栏导航组件 -->
 <template>
 	<div class="left_bar">
-		<el-menu :router="true" unique-opened>
+		<el-menu
+			class="el_menu"
+			background-color="#fff"
+			text-color="#000"
+			active-text-color="#fff"
+			:default-active="$route.path"
+			:router="true"
+		>
 			<el-menu-item index="/user">管理用户</el-menu-item>
 			<el-menu-item index="/server">管理服务</el-menu-item>
 			<el-menu-item index="/mail">群发邮件</el-menu-item>
@@ -19,4 +27,14 @@
 	};
 </script>
 
-<style scoped></style>
+<style scoped>
+	:deep(.is-active) {
+		background-color: rgba(91, 133, 253, 1) !important;
+	}
+
+	.el_menu {
+		width: 200px;
+		height: 100%;
+		border-right: 1px solid #e4e7ed;
+	}
+</style>
